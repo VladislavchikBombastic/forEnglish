ca0cadcfa15df98cb71e7b10b3ba808e2ed52f77
VladislavchikBombastic <160402480+VladislavchikBombastic@users.noreply.github.com>
2026-04-30 08:07:09 +0300

    Add functions for addition and subtraction operations

---
+++ math_operations.py
@@ -0,0 +1,9 @@
+def calculate_addition(first_addend: float, 
+                       second_addend: float) -> float:
+    calculation_sum = first_addend + second_addend
+    return calculation_sum
+
+def calculate_subtraction(minuend_value: float, 
+                          subtrahend_value: float) -> float:
+    result_difference = minuend_value - subtrahend_value
+    return result_difference
